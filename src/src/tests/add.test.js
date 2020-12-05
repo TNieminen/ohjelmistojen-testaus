4373138e2c5c846c73bee719a9be2ce178203c2c
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-05 16:39:09 +0800

    chore: verified test cases of add.js

--- src/src/tests/add.test.js
+++ src/src/tests/add.test.js
@@ -1,16 +1,6 @@
 import add from '@/add.js'
 
 describe('==== add ====', () => {
-
-  beforeAll(() => {
-    // TYPE: SETUP, run global setups at the start if needed
-  })
-  beforeEach(() => {
-    // TYPE: SETUP, run a script before each "it"
-  })
-  afterEach(() => {
-    // TYPE: CLEAN UP, run scripts to clean up after each "it"
-  })
  
   it('Should return 10 if inputs are 4 and 6',() => {
     expect(add(4,6)).toEqual(10)
@@ -24,10 +14,17 @@ describe('==== add ====', () => {
   it('Should return 2 if input -4 and 6',() => {
     expect(add(-4,6)).toEqual(2)
   })
-  xit('Should return 0 if inputs are invalid and valid',() => {
-    expect(add('invalid',6)).toEqual(0)
+  it('Should work with floating point numbers',() => {
+    expect(add(1.1,1.9)).toEqual(3)
   })
-  xit('Should return 0 if inputs are invalid and invalid',() => {
-    expect(add('invalid','invalid')).toEqual(0)
+  // TODO: The function lack input validation and will return unintended results.
+  // Since the library is not typed inputs should be validated
+  xit('Should throw an error if inputs are invalid and valid',() => {
+    expect(() => add('invalid',6)).toThrow()
+  })
+  // TODO: The function lack input validation and will return unintended results.
+  // Since the library is not typed inputs should be validated
+  xit('Should throw an error if inputs are not valid numbers',() => {
+    expect(() => add('invalid','invalid')).toThrow()
   })
 })
