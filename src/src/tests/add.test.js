f3e0a5e852d4d7da7ef997d4dcea425678c8c6f4
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-05 17:01:22 +0800

    chore: added reasoning for input validation

--- src/src/tests/add.test.js
+++ src/src/tests/add.test.js
@@ -28,3 +28,14 @@ describe('==== add ====', () => {
     expect(() => add('invalid','invalid')).toThrow()
   })
 })
+
+/**
+ * The philosophy behind when we should require input validation in a non-ts environment is as follows:
+ * 1. If I don't do input validation, can it lead to unintended results and hard to debug cases?
+ * 2. If yes, does adding input validation with custom errors increase the amount of information to the user of the function?
+ * For instance if we expect an array as input and we call forEach on it, we don't need to verify if the input is an array because
+ * forEach is only defined for arrays and will throw a well defined error.
+ * 3. If yes, add input validation
+ * 
+ * At the very least the lack of input validation and default returns in those cases should be documented in the function
+ */
