57346d87379abf7a93878cd839abbda67357134a
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-06 16:26:52 +0800

    optimize compact tests

--- src/src/tests/compact.test.js
+++ src/src/tests/compact.test.js
@@ -1,27 +1,52 @@
 import compact from '@/compact.js'
 
 describe('==== compact ====', () => {
-
-  beforeAll(() => {
-    // TYPE: SETUP, run global setups at the start if needed
-  })
-  beforeEach(() => {
-    // TYPE: SETUP, run a script before each "it"
-  })
-  afterEach(() => {
-    // TYPE: CLEAN UP, run scripts to clean up after each "it"
-  })
   
-  xit('Should return ignore falsey values in input',() => {
-    expect(compact([0,1,null,2,3,false])).toEqual([1,2,3])
+  // TODO: absolutely not ready for usage, doesn't give any of it's promised functionality
+  
+  xit('Should ignore null in input',() => {
+    expect(compact([null,1,2,3])).toEqual([1,2,3])
   })
 
-  xit('Should return array with no falsey values in input',() => {
+  xit('Should ignore false in input',() => {
+    expect(compact([false,1,2,3])).toEqual([1,2,3])
+  })
+
+  xit('Should ignore 0 in input',() => {
+    expect(compact([0,1,2,3])).toEqual([1,2,3])
+  })
+
+  xit('Should ignore empty string in input',() => {
+    expect(compact(['',1,2,3])).toEqual([1,2,3])
+  })
+
+  xit('Should ignore undefined in input',() => {
+    expect(compact([undefined,1,2,3])).toEqual([1,2,3])
+  })
+
+  xit('Should ignore NaN in input',() => {
+    expect(compact([NaN,1,2,3])).toEqual([1,2,3])
+  })
+
+  xit('Should filter all falsy values from end of the array',() => {
+    expect(compact([1,2,3,0,NaN,undefined,null,''])).toEqual([1,2,3])
+  })
+
+  xit('Should filter all falsy values from start of the array',() => {
+    expect(compact([NaN,undefined,null,'',0,1,2,3])).toEqual([1,2,3])
+  })
+
+  xit('Should filter all falsy values with no regard to position',() => {
+    expect(compact([NaN,1,undefined,2,null,'',0,3])).toEqual([1,2,3])
+  })
+
+
+  xit('Should return original array with no falsy values in input',() => {
     expect(compact([1,2,3,4,5])).toEqual([1,2,3,4,5])
   })
 
   // neg cases
-  it('Should return empty with only falsey input',() => {
+  it('Should return empty with only falsy input',() => {
     expect(compact([0,null,undefined,false,'',NaN])).toEqual([])
   })
 
