96542a73bdb3866db33d4083d3897b91da9a62a3
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-06 16:28:49 +0800

    optimize: endswith tests

--- src/src/tests/endsWith.test.js
+++ src/src/tests/endsWith.test.js
@@ -1,21 +1,11 @@
 import endsWith from '@/endsWith.js'
 
 describe('==== endsWith ====', () => {
-
-  beforeAll(() => {
-    // TYPE: SETUP, run global setups at the start if needed
-  })
-  beforeEach(() => {
-    // TYPE: SETUP, run a script before each "it"
-  })
-  afterEach(() => {
-    // TYPE: CLEAN UP, run scripts to clean up after each "it"
-  })
  
-  it('Should return true when ends with same',() => {
+  it('Should return true when ends with same with no position stated',() => {
     expect(endsWith('1234','4')).toEqual(true)
   })
-  it('Should return false when not same end',() => {
+  it('Should return false when not same end with no position stated',() => {
     expect(endsWith('1234','3')).toEqual(false)
   })
   it('Should return true when position stated',() => {
@@ -23,5 +13,27 @@ describe('==== endsWith ====', () => {
   })
   it('Should return false when position is wrong',() => {
     expect(endsWith('1234','4',3)).toEqual(false)
+  })
+
+  /**
+   * TODO: There is no documentation for special case handling, hence
+   * we would expect these to throw errors otherwise the program might
+   * end up in an unknown state. If not fixed the documentation should at least be updated
+   */
+  it('Should throw error with null input',() => {
+    expect(() => endsWith(null,'4')).toThrow()
+  })
+  it('Should throw error with undefined input',() => {
+    expect(() => endsWith(undefined,'4')).toThrow()
+  })
+  xit('Should throw error with NaN input',() => {
+    expect(() => endsWith(NaN,'4')).toThrow()
+  })
+  xit('Should throw error with object input',() => {
+    expect(() => endsWith({},'4')).toThrow()
+  })
+  xit('Should throw error with function input',() => {
+    const testFunc = () => {}
+    expect(() => endsWith(testFunc,'4')).toThrow()
   })
 })
