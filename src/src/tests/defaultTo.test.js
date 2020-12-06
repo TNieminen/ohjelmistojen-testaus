f81a0f4e0152081bd9e29405e07c24496149cc18
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-06 16:27:31 +0800

    optimize: defaultto tests

--- src/src/tests/defaultTo.test.js
+++ src/src/tests/defaultTo.test.js
@@ -1,32 +1,32 @@
 import defaultTo from '@/defaultTo.js'
 
 describe('==== defaultTo ====', () => {
-    
-  beforeAll(() => {
-    // TYPE: SETUP, run global setups at the start if needed
-  })
-  beforeEach(() => {
-    // TYPE: SETUP, run a script before each "it"
-  })
-  afterEach(() => {
-    // TYPE: CLEAN UP, run scripts to clean up after each "it"
-  })
   
-  it('Should return value with string input',() => {
+  it('Should return original value with string input',() => {
     expect(defaultTo('valid','default')).toEqual('valid')
   })
 
-  it('Should return default value with number input',() => {
+  it('Should return original integer input',() => {
     expect(defaultTo(1,'default')).toEqual(1)
   })
 
   // neg cases
-  it('Should return default value with falsey input',() => {
+  it('Should return default value with null input',() => {
     expect(defaultTo(null,'default')).toEqual('default')
   })
 
-  it('Should return default value with falsey input',() => {
+  it('Should return default value with undefined input',() => {
     expect(defaultTo(undefined,'default')).toEqual('default')
   })
 
+  // TODO: it seems we are comparing against null, NaN will not pass this test
+  // because it is a special case in comparisons
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN
+  xit('Should return default value with NaN input',() => {
+    expect(defaultTo(NaN,'default')).toEqual('default')
+  })
+
+  it('Should return original value with empty string input',() => {
+    expect(defaultTo('','default')).toEqual('')
+  })
 })
