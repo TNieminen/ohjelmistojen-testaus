15ef3d6fa44f4a50a7350614f11536ac31191b7f
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-07 14:48:31 +0800

    optimize: remade memoize tests

--- src/src/tests/memoize.test.js
+++ src/src/tests/memoize.test.js
@@ -1,30 +1,51 @@
 import memoize from '@/memoize.js'
 
 describe('==== memoize ====', () => {
-
-  beforeEach(() => {
-    const object = {'a':1,'b':2};
-    const other = {'c':3,'d':4};
-  })
      
-  it('Should provide correct values when given object',() => {
-    const values = memoize(values)
-    expect(values(object)).toEqual([1,2])
+  beforeEach(() => {
+    memoize.Cache = undefined
+    memoize.cache = undefined
+  })
+  it('Should handle single input and return correct value',() => {
+    const memoized = memoize(a => Math.pow(a,2))
+    expect(memoized(2)).toEqual(4)
   })
 
-  it('Should provide correct values when given other object',() => {
-    const values = memoize(values)
-    expect(values(other)).toEqual([3,4])
+  it('Should handle single input and save it in cache',() => {
+    const memoized = memoize(a => Math.pow(a,2))
+    memoized(2)
+    expect(memoized.cache.has(2)).toEqual(true) // cache is documented as freely accessible and modifiable
   })
 
-  it('Should provide correct values when given object with changes',() => {
-    const values = memoize(values)
-    object.a = 2
-    expect(values(object)).toEqual([1,2])
+  it('Should handle single input and return it from cache in the future',() => {
+    const mockFunc = jest.fn(a => Math.pow(a,2))
+    const memoized = memoize(mockFunc)
+    memoized(2)
+    expect(memoized(2)).toEqual(4)
+    expect(mockFunc).toHaveBeenCalledTimes(1)    
+    mockFunc.mockClear()
   })
-  it('Should provide correct values when cache modified',() => {
-    const values = memoize(values)
-    values.cache.set(object, ['a','b'])
-    expect(values(other)).toEqual(['a','b'])
+
+  it('Should allow changing the Cache constructor',() => {
+    class testCache {
+      constructor() {
+        this.test = true
+      }
+    }
+    memoize.Cache = testCache
+    const memoized = memoize(a => Math.pow(a,2))
+    expect(memoized.cache.test).toEqual(true)
   })
+
+  it('Should throw error if Cache constructor is invalid',() => {
+    class testCache {
+      constructor() {
+        this.test = true
+      }
+    }
+    memoize.Cache = testCache
+    const memoized = memoize(a => Math.pow(a,2))
+    expect(() => memoized(2)).toThrow()
+  })
+
 })
