9a6a6b850970caf3bd1b985584e1b8100c331c9c
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-07 14:57:09 +0800

    chore: added some comments

--- src/src/tests/countBy.test.js
+++ src/src/tests/countBy.test.js
@@ -7,11 +7,13 @@ describe('==== countBy ====', () => {
     {'user': 'name2', 'val': true},
     {'user': 'name3', 'val': false}]
   
+  // TODO: Returns incorrectly {false:0, true:1}
   xit('Should return correct amount of values in input',() => {
     let ans = {'true':2,'false':1}
     expect(countBy(users,value => value.val)).toEqual(ans)
   })
 
+  // TODO: Returns incorrectly {name1:0, name2:0, name3:0}
   xit('Should return correct amount of names in input',() => {
     let ans = {'name1':1,'name2':1,'name3':1}
     expect(countBy(users,value => value.user)).toEqual(ans)
