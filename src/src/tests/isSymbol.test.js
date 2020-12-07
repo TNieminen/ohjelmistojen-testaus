8a770da8db471820e4fe27e06d9a43c4e1bd2bbe
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-07 14:47:20 +0800

    optimize: issymbol tests

--- src/src/tests/isSymbol.test.js
+++ src/src/tests/isSymbol.test.js
@@ -12,16 +12,16 @@ describe('==== isSymbol ====', () => {
     expect(isSymbol(NaN)).toEqual(false)
   })
   it('Should return false if input is an Function',() => {
-    expect(isSymbol(Function)).toEqual(false)
+    expect(isSymbol(() => {})).toEqual(false)
   })
   it('Should return true if input is an Symbol',() => {
     expect(isSymbol(Symbol())).toEqual(true)
   })
   it('Should return true if input is an Symbol iterator',() => {
-    expect(isSymbol(Symbol().iterator)).toEqual(true)
+    expect(isSymbol(Symbol.iterator)).toEqual(true)
   })
   it('Should return false if input is string',() => {
-    expect(isSymbol("test")).toEqual(false)
+    expect(isSymbol('test')).toEqual(false)
   })
   it('Should return false if input is number',() => {
     expect(isSymbol(10)).toEqual(false)
