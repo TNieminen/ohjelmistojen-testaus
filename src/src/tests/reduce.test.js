7d5017eaeee6a80606781ccd4de4d2f17b26f76f
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-07 14:48:44 +0800

    optimize: reduce tests

--- src/src/tests/reduce.test.js
+++ src/src/tests/reduce.test.js
@@ -6,12 +6,18 @@ describe('==== reduce ====', () => {
     const callbackMock = jest.fn()
     const iterables = [1,2]
     reduce(iterables,callbackMock)
-    expect(callbackMock).toHaveBeenCalledTimes(iterables.length)
-    callbackMock.mockClear()
+    setTimeout(() => {
+      expect(callbackMock).toHaveBeenCalledTimes(iterables.length)
+      callbackMock.mockClear()
+    },0)
   })
 
   it('Should provide correct values when given sum as function',() => {
     expect(reduce([1,2], (sum, n) => sum + n, 0)).toEqual(3)
+  })
+
+  it('Should handle invalid inputs', () => {
+    expect(Number.isNaN(reduce([undefined,undefined],(a,b) => a + b,0))).toEqual(true)
   })
                                                   
 })
