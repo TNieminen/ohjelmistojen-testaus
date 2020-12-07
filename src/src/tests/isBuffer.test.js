5b1b26b7562cf453d6fc6cbd482c8bf4a1f3f941
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-07 14:46:20 +0800

    optimize: isbuffer tests

--- src/src/tests/isBuffer.test.js
+++ src/src/tests/isBuffer.test.js
@@ -1,5 +1,11 @@
 import isBuffer from '@/isBuffer.js'
 
+/**
+ * Buffer is a nodejs class and not available in the target environment. 
+ * To make this class work user would have to use a polyfill
+ * @see https://github.com/feross/buffer
+ * @see https://nodejs.org/api/buffer.html#buffer_buffer
+ */
 describe('==== isBuffer ====', () => {
   
   it('Should return false if input is undefined',() => {
@@ -27,9 +33,18 @@ describe('==== isBuffer ====', () => {
     expect(isBuffer(() => {})).toEqual(false)
   })
   it('Should return false if input is a Uint8Array',() => {
-    expect(isBuffer(new Uint8Array())).toEqual(true)
+    expect(isBuffer(new Uint8Array(2))).toEqual(false)
   })
-  it('Should return true with Buffer constructor',() => {
-    expect(isBuffer(new Buffer())).toEqual(true)
+  it('Should return true with deprecated Buffer constructor',() => {
+    expect(isBuffer(new Buffer(2))).toEqual(true)
+  })
+  it('Should return true with Buffer.alloc constructor',() => {
+    expect(isBuffer(Buffer.alloc(2))).toEqual(true)
+  })
+  it('Should return true with Buffer.from constructor',() => {
+    expect(isBuffer(Buffer.from('2'))).toEqual(true)
+  })
+  it('Should return false with ArrayBuffer constructor',() => {
+    expect(isBuffer(new ArrayBuffer(2))).toEqual(false)
   })
 })
