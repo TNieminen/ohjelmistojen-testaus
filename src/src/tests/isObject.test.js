3ca502c6a1c1586cebddaf65ce01e92a67fa395a
Tuomo Nieminen <tuomo.aa.nieminen@gmail.com>
2020-12-07 14:47:07 +0800

    optimize: isobject tests

--- src/src/tests/isObject.test.js
+++ src/src/tests/isObject.test.js
@@ -12,7 +12,7 @@ describe('==== isObject ====', () => {
     expect(isObject(NaN)).toEqual(false)
   })
   it('Should return true if input is an Function',() => {
-    expect(isObject(Function)).toEqual(true)
+    expect(isObject(() => {})).toEqual(true)
   })
   it('Should return true if input is an Array',() => {
     expect(isObject([])).toEqual(true)
@@ -26,8 +26,20 @@ describe('==== isObject ====', () => {
   it('Should return true if input is an new String',() => {
     expect(isObject(new String(''))).toEqual(true)
   })
+  it('Should return true if input is an new Boolean',() => {
+    expect(isObject(new Boolean())).toEqual(true)
+  })
+  it('Should return true if input is an new Map',() => {
+    expect(isObject(new Map())).toEqual(true)
+  })
+  it('Should return true if input is an new Set',() => {
+    expect(isObject(new Set())).toEqual(true)
+  })
+  it('Should return true if input is an new Regexp',() => {
+    expect(isObject(new RegExp(/test/))).toEqual(true)
+  })
   it('Should return false if input is string',() => {
-    expect(isObject("test")).toEqual(false)
+    expect(isObject('test')).toEqual(false)
   })
   it('Should return false if input is number',() => {
     expect(isObject(10)).toEqual(false)
